f977e3a2a2326d061c0dc11378c7a00336c2555a
Manabu WATANABE <harumanachika@users.noreply.github.com>
2019-11-06 22:54:54 +0900

    update

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define _GLIBCXX_DEBUG
+#define int int64_t
+#define double long double
+using namespace std;
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	
+	//cout <<  << endl;
+	return 0;
+}
+//std::string str;
+//str = std::to_string(int i);
+//bitset<3> s(N);
+//int ans = (char)grid - '0';
