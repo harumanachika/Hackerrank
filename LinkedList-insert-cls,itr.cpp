29a71079041c701a68c160a64407d96d131e9cbb
harumanachika <harumanachika@users.noreply.github.com>
2020-04-12 19:28:42 +0900

    update

--- LinkedList-insert-cls,itr.cpp
+++ LinkedList-insert-cls,itr.cpp
@@ -157,6 +157,22 @@ bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
 	}
 }
 
+//merge Two Sorted
+SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+	if(head1 == NULL) {
+		return head2;
+	} else if(head2 == NULL) {
+		return head1;
+	}
+	
+	if(head1->data < head2->data) {
+		head1->next = mergeLists(head1->next, head2);
+		return head1;
+	} else {
+		head2->next = mergeLists(head1, head2->next);
+		return head2;
+	}
+}
 
 /* メイン・プログラム */
 signed main() {
